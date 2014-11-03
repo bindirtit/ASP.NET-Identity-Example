9388c74c5041660b5c8cf358936b4179172c71df
Phuc Thai <bindirtit@gmail.com>
2014-11-03 15:41:52 +0700

    Chapter 13, page 316. Create User and show list of user

---
+++ ASP.NET Identity Sample/Users/Controllers/AdminController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Users.Infrastructure;
+using Microsoft.AspNet.Identity.Owin;
+using Users.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace Users.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+
+            return View(UserManager.Users);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                AppUser user = new AppUser()
+                {
+                    Email = model.Email,
+                    UserName = model.Name
+                };
+
+                IdentityResult result = await UserManager.CreateAsync(
+                    user:user, 
+                    password: model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                this.ModelState.AddModelError("", error);
+            }
+        }
+
+        private AppUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+        }
+    }
+}
